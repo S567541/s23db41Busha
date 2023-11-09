0cc5bf37c15a8384c7ab6357c4eae17c88d49dd3
S567541 <S567541@nwmissouri.edu>
2023-11-08 20:48:42 -0600

    commit

--- controllers/whistle.js
+++ controllers/whistle.js
@@ -8,8 +8,24 @@ exports.whistle_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: whistle detail: ' + req.params.id);
 };
 // Handle whistle create on POST.
-exports.whistle_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: whistle create POST');
+exports.whistle_create_post = async function(req, res) {
+    console.log(req.body)
+let document = new Costume();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.costume_type = req.body.costume_type;
+document.cost = req.body.cost;
+document.size = req.body.size;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
 };
 // Handle whistle delete form on DELETE.
 exports.whistle_delete = function(req, res) {
@@ -23,6 +39,19 @@ exports.whistle_list = async function(req, res) {
     try{
     thewhistle = await whistle.find();
     res.send(thewhistle);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
+    //VIEWS
+    // Handle a show all view
+    exports.whistle_view_all_Page = async function(req, res) {
+    try{
+    thewhistles = await whistle.find();
+    res.render('whistle', { title: 'whistle Search Results', whistlelist: thewhistles });
     }
     catch(err){
     res.status(500);
--- routes/Whistle.js
+++ routes/Whistle.js
@@ -1,9 +1,7 @@
 var express = require('express');
+const whistle_controlers= require('../controllers/whistle');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Whistle', { title: 'Search Results for Whistle' });
-});
-
-module.exports = router;
+  router.get('/', whistle_controlers.whistle_view_all_Page );
+  module.exports = router
