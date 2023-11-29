f53d8ce61f112f7cd2db671e8f0e43d41b9e32a6
S567541 <S567541@nwmissouri.edu>
2023-11-29 11:48:17 -0600

    part 5 changes

--- models/whistle.js
+++ models/whistle.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const whistleSchema = mongoose.Schema({
     WhistleId: String,
-    Whistle_Price: Number,
+    Whistle_Price: {
+        type: Number,
+        min: 0,
+        max:5000
+    },
     Whistle_Style:String
 })
 module.exports = mongoose.model("whistle",
--- routes/Whistle.js
+++ routes/Whistle.js
@@ -5,6 +5,16 @@ const whistlecreate_controller= require('../controllers/whistlecreate');
 const whistleupdate_controller= require('../controllers/whistleupdate');
 const whistledelete_controller= require('../controllers/whistledelete');
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 var router = express.Router();
 
 /* GET home page. */
@@ -12,7 +22,7 @@ var router = express.Router();
 /* GET detail costume page */
 router.get('/detail', whistledetail_controlers.whistle_view_one_Page);
 router.get('/create', whistlecreate_controller.whistle_create_Page);
-router.get('/update', whistleupdate_controller.whistle_update_Page);
+router.get('/update',secured, whistleupdate_controller.whistle_update_Page);
 router.get('/delete', whistledelete_controller.whistle_delete_Page);
 
 
